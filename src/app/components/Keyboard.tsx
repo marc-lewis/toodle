5616ef6bffa058c1f1a5961edca3093f592271f6
Marc Lewis <marc@marclewis.io>
2024-07-19 22:37:28 +0100

    task: add basic layout to Keyboard.tsx

--- src/app/components/Keyboard.tsx
+++ src/app/components/Keyboard.tsx
@@ -3,9 +3,34 @@
  * @returns The Keyboard component
  */
 export default function Keyboard () {
+  const LetterRows = [
+    ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "⌫"],
+    ["A", "S", "D", "F", "G", "H", "J", "K", "L", "↵"],
+    ["Z", "X", "C", "V", "B", "N", "M"],
+  ]
   return (
-    <div>
-      <h1>Keyboard</h1>
-    </div>
+    <section className="flex justify-center">
+      <div className="flex flex-col">
+      {LetterRows.map((row, i) => {
+        return (
+          <div
+            className={`flex flex-row gap-1 m-1 pl-${i * 4}`}
+            key={i}
+          >
+            {row.map((letter, j) => {
+              return (
+                <button
+                  className="py-2 px-2 border border-black rounded-lg"
+                  key={j}
+                >
+                  {letter}
+                </button>
+              )
+            })}
+          </div>
+        )
+      })}
+      </div>
+    </section>
   )
 }
